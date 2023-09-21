f6a9f2cc1e52b67a86e017ea75e45a98efcd4377
AndreiH96 <huzunandrei96@gmail.com>
2023-09-21 23:25:52 +0300

    First comit

---
+++ index.js
@@ -0,0 +1,57 @@
+//ex1
+
+let str = 'safasdsada'
+const namber = 212121
+var bool = true
+let surname
+let big = 454545645456456456456456456
+let name = null
+let obj = {}
+let arr = []
+
+console.log(typeof str)
+console.log(typeof namber)
+console.log(typeof bool)
+console.log(typeof surname)
+console.log(typeof big)
+console.log(typeof name)
+console.log(typeof obj)
+console.log(typeof arr)
+
+// ex2
+let num1 = 5
+let num2 = 10
+let num3 = 15
+
+let suma = num1 + num2 + num3
+let maiMare = num2  > num1
+let maiMareEgal  = num1 >= num2
+let maiMic = num3 < num1 
+let maiMicEgal = num1 <= num3
+console.log(suma)
+console.log(maiMare)
+console.log(maiMareEgal)
+console.log(maiMic)
+console.log(maiMicEgal)
+
+
+//ex3
+
+let a = 'Hello'
+let b = 'World'
+
+console.log(a  +' '+  b)
+
+
+//ex4 
+
+let obj1  = {
+    nume: 'Andrei',
+    prenume: 'Huzun',
+    virsta: '24 ani',
+    hobby: 'Sah',
+    profesie: 'Web Developer',
+
+}
+
+console.log(obj1)
